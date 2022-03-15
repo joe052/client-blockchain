dba50043327a20688038d8931be83df006e38e1f
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-03-15 13:37:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//https://kipalog.com/posts/How-to-build-a-Decentralized-Blockchain-Network-in-Javascript
+//import fetch from "node-fetch";
+//import * as crypto from 'crypto';
+//const bb = require('./newProject/complete.js');
+//const bb = require('./ready/progress.js');
+//const bb2 = require('./newProject/progression.js');
+const bb1 = require('./newProject/server.js');
+//const bb1 = require('./newProject/front.js');
+//const bb1 = require('./newProject/complex.js');
---
+++ newProject/server.js
@@ -0,0 +1,53 @@
+const chain = require("./progression.js");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+const courses = [
+  {id: 1, name: 'maths'},
+  {id: 2, name: 'science'},
+  {id: 3, name: 'education'}
+];
+
+app.use(bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}));
+app.use(express.json());
+
+app.use(express.static('newProject'));
+app.listen(port,()=>{
+  console.log(`app listening on port ${port}`);
+});
+
+app.post('/data',(req,res)=>{
+  console.log(req.body);
+});
+
+app.get('/',(req,res)=>{
+  res.send('Build in progress.. @Client');
+});
+
+app.get('/api/courses',(req,res)=>{
+  res.send(courses);
+});
+
+app.get('/api/courses/:id',(req,res)=>{
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if(!course) res.status(404).send('files not found!');
+  res.send(course);
+});
+
+app.post('/api/courses',(req,res)=>{
+  const course = {
+    id: courses.length +1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+  console.log(courses);
+});
+
+app.get('/blockchain',(req,res)=>{
+  res.send(chain[0]);
+});
+
+//console.log(chain);
